2df830c71da9c97d553f34cdd11458b3dcf03084
edwinnab <edwinnabikeri@gmail.com>
2022-12-06 16:52:58 +0300

    week1 code challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
-// Your code here
+function displayCharacterNames(character) {
+   let span = document.createElement("span")
+   span.innerHTML = `
+   <p id="cName">${character.name}<p>
+   `
+   document.getElementById("character-bar").appendChild(span)
+
+   let characterObj = {
+    id: character.id,
+    name: character.name,
+    image: character.image,
+    votes: character.votes
+   }
+
+   const cardName = span.querySelector("#cName")
+   cardName.addEventListener("click", () => {
+    document.querySelector("#name").textContent = character.name
+    document.querySelector("#image").src = character.image
+    document.querySelector("#vote-count").textContent = character.votes
+   })
+
+   let form = document.querySelector("#votes-form")
+   form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    character.votes = e.target.votes.value
+    document.querySelector("#vote-count").textContent = character.votes
+    updateDB(character)
+   })
+}
+
+//GET Requests
+function getData() {
+    fetch("http://localhost:3000/characters")
+    .then(res => res.json())
+    .then(data => data.forEach(character => displayCharacterNames(character)))
+ }
+ 
+ getData()
+
+function updateDB(characterObj) {
+    fetch(`http://localhost:3000/characters/${characterObj.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(characterObj)
+    })
+}
+
+function resetVotes() {
+    let resetButton = document.querySelector("#reset-btn")
+    resetButton.addEventListener("click", () => {
+        document.querySelector("#vote-count").textContent = 0
+    })
+}
+
+resetVotes()
+ 
+
+
+
+
+
